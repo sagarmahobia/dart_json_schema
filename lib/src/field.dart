683fd14cfcc4d66f10d6bc8120e9370fc70b0c9f
Sagar Mahobia <sagarmahobia5@gmail.com>
2025-08-02 15:35:23 +0530

    Replace Field annotation with external package import
    Refactors the Field annotation implementation to use the dart_json_schema_annotations package instead of maintaining a local implementation.
    
    Adds .idea directory to gitignore and includes new package dependency in pubspec.yaml.

--- lib/src/field.dart
+++ lib/src/field.dart
@@ -1,19 +1,2 @@
-
-/// An annotation to add metadata to model fields for JSON schema generation.
-class Field {
-  /// A human-readable title for the field.
-  final String? title;
-
-  /// A description of the field.
-  final String? description;
-
-  /// A list of example values for the field.
-  final List<dynamic>? examples;
-
-  /// Creates a new [Field] annotation.
-  const Field({
-    this.title,
-    this.description,
-    this.examples,
-  });
-}
+/// Re-exporting the Field annotation from the dart_json_schema_annotations package.
+export 'package:dart_json_schema_annotations/dart_json_schema_annotations.dart' show Field;
